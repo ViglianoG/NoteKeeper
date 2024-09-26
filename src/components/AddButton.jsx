93959d186358e0b5af3e6f46c87f3a6b0a9474cb
ViglianoG <gastonvigliano@gmail.com>
2024-09-26 00:51:48 -0300

    addex context to add button

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -2,8 +2,11 @@ import Plus from "../icons/Plus.jsx";
 import colors from "../assets/colors.json";
 import { useRef } from "react";
 import { db } from "../appwrite/databases.js";
+import { useContext } from "react";
+import { NoteContext } from "../context/NoteContext.jsx";
 
 const AddButton = () => {
+  const { setNotes } = useContext(NoteContext);
   const startingPosition = useRef(10);
 
   const addNote = async () => {
@@ -16,7 +19,9 @@ const AddButton = () => {
     };
     startingPosition.current += 10;
     const response = await db.notes.create(payload);
+    setNotes((prevState) => [response, ...prevState]);
   };
+
   return (
     <div id="add-btn" onClick={addNote}>
       <Plus />
